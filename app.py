a961cf91fe634dffee0e573038467d8987078862
Parag0506 <paragghorpade1998@gmail.com>
2020-03-25 23:15:14 +0530

    Web app addition

--- app.py
+++ app.py
@@ -155,6 +155,6 @@ def upload():
 
 
 port = int(os.environ.get('PORT', 5000))
-app.run(host="127.0.0.1", port=port, debug=True)
+app.run(host="0.0.0.0", port=port, debug=True)
 #if __name__ == '__main__':
 #    app.run()
